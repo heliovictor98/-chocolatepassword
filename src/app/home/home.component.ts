8b3af65e76b26b935e69793419a5c081aaede123
helio <barros.helio98@gmail.com>
2024-07-23 22:50:32 -0300

    Layout pronto e metodo de geração pre moldado

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  quantidadeCaractere: number = 3; // Valor inicial
 
+  senhaGerada: boolean = false;
+
+  gerarSenha() {
+    this.senhaGerada = true;
+  }
+
+  constructor() { }
 }
