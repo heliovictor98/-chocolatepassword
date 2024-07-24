cfbd9c2cf4308f406b25d869e77b00640e0fdff5
helio <barros.helio98@gmail.com>
2024-07-24 20:08:06 -0300

    Logica para GERAR SENHA e BTN de COPY

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,14 +12,46 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  quantidadeCaractere: number = 3; // Valor inicial
 
   senhaGerada: boolean = false;
   loading: boolean = false;
   btnGerar: boolean = true;
   msgSucesso: boolean = false;
 
+  password = '';
+
+  //DADOS DO FORM OBJS
+  inputMaiusculo: boolean = false;
+  inputMinuscula: boolean = false;
+  inputNumero: boolean = true;
+  inputEspecial: boolean = false;
+  quantidadeCaractere: number = 3;
+
   gerarSenha() {
+    const upperCaseCharset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerCaseCharset = 'abcdefghijklmnopqrstuvwxyz';
+    const numberCharset = '0123456789';
+    const symbolCharset = '!@#$%^&*()_+~`|}{[]:;?><,./-=';
+
+    let charset = '';
+    if (this.inputMaiusculo) charset += upperCaseCharset;
+    if (this.inputMinuscula) charset += lowerCaseCharset;
+    if (this.inputNumero) charset += numberCharset;
+    if (this.inputEspecial) charset += symbolCharset;
+
+    if (charset === '') {
+      setTimeout(() => {
+       console.log("teste")
+      }, 500);
+    }
+
+    let generatedPassword = '';
+    for (let i = 0, n = charset.length; i < this.quantidadeCaractere; ++i) {
+      generatedPassword += charset.charAt(Math.floor(Math.random() * n));
+      this.password = generatedPassword;
+    }
+    console.log(this.password);
+
       this.loading = true;
       this.btnGerar = false;
       this.senhaGerada = false;
@@ -43,6 +75,9 @@ export class HomeComponent {
 
   btnCopy(){
     this.msgSucesso = true;
+    if(this.password != null || this.password != null){
+      navigator.clipboard.writeText(this.password);
+    }
     setTimeout(() =>{
       this.msgSucesso = false;
     },5000)
