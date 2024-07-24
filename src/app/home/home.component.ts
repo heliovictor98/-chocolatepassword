8e9d05b574e49d50591c3a249bb5cf3027a757e0
helio <barros.helio98@gmail.com>
2024-07-24 10:38:48 -0700

    att

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,9 +15,37 @@ export class HomeComponent {
   quantidadeCaractere: number = 3; // Valor inicial
 
   senhaGerada: boolean = false;
+  loading: boolean = false;
+  btnGerar: boolean = true;
+  msgSucesso: boolean = false;
 
   gerarSenha() {
-    this.senhaGerada = true;
+      this.loading = true;
+      this.btnGerar = false;
+      this.senhaGerada = false;
+    setTimeout(() =>{
+      this.loading = false;
+      this.btnGerar = true;
+      this.senhaGerada = true;
+    },1500)
+
+  }
+
+  onQuantidadeCaractereChange(value: number): void {
+    if (value < 3) {
+      this.quantidadeCaractere = 3;
+    } else if (value > 36) {
+      this.quantidadeCaractere = 36;
+    } else {
+      this.quantidadeCaractere = value;
+    }
+  }
+
+  btnCopy(){
+    this.msgSucesso = true;
+    setTimeout(() =>{
+      this.msgSucesso = false;
+    },5000)
   }
 
   constructor() { }
