b44dfbb92813c8a0510c159abb1c8c49d4ca723b
heliobarros <barros.helio98@gmail.com>
2024-07-24 22:53:57 -0300

    att v angular

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
+import { LottieModule } from 'ngx-lottie';
 @Component({
   selector: 'app-home',
   standalone: true,
